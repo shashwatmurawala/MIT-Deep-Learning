deb87a2e9ae4453a22b243ea0a069dde36402390
Shashwat Murawala <murawala.shashwat@gmail.com>
2026-01-16 23:14:06 +0900

    Finished 2.3

--- SoftwareLab1/lab1p2.py
+++ SoftwareLab1/lab1p2.py
@@ -1,6 +1,6 @@
 import comet_ml
 # TODO: ENTER YOUR API KEY HERE!! instructions above
-COMET_API_KEY = ""
+COMET_API_KEY = "oFWvyXLDIJkiE8xP5bR4unBnM"
 
 # Import PyTorch and other relevant libraries
 import torch
@@ -25,7 +25,6 @@ songs = mdl.lab1.load_training_data()
 # Print one of the songs to inspect it in greater detail!
 songs_joined = "\n\n".join(songs)
 vocab = sorted(set(songs_joined))
-print("There are", len(vocab), "unique characters in the dataset")
 
 ### Define numerical representation of text ###
 
@@ -38,7 +37,6 @@ char2idx = {u: i for i, u in enumerate(vocab)}
 #   the inverse of char2idx and allows us to convert back
 #   from unique index to the character in our vocabulary.
 idx2char = np.array(vocab)
-
 '''TODO: Write a function to convert the all songs string to a vectorized
     (i.e., numeric) representation. Use the appropriate mapping
     above to convert from vocab characters to the corresponding indices.
@@ -48,6 +46,39 @@ idx2char = np.array(vocab)
   the number of characters in the input string
 '''
 def vectorize_string(string):
-  '''TODO'''
+  vectorized = np.array([char2idx[char] for char in string])
+  return vectorized
 
 vectorized_songs = vectorize_string(songs_joined)
+
+### Batch definition to create training examples ###
+
+def get_batch(vectorized_songs, seq_length, batch_size):
+    # the length of the vectorized songs string
+    n = vectorized_songs.shape[0] - 1
+    # randomly choose the starting indices for the examples in the training batch
+    idx = np.random.choice(n - seq_length, batch_size)
+
+    '''TODO: construct a list of input sequences for the training batch'''
+    input_batch = np.array([vectorized_songs[i:i+seq_length] for i in idx])
+
+    '''TODO: construct a list of output sequences for the training batch'''
+    output_batch = np.array([vectorized_songs[i+1:i+seq_length+1] for i in idx])
+
+    # Convert the input and output batches to tensors
+    x_batch = torch.tensor(input_batch, dtype=torch.long)
+    y_batch = torch.tensor(output_batch, dtype=torch.long)
+
+    return x_batch, y_batch
+
+# Perform some simple tests to make sure your batch function is working properly!
+test_args = (vectorized_songs, 10, 2)
+x_batch, y_batch = get_batch(*test_args)
+assert x_batch.shape == (2, 10), "x_batch shape is incorrect"
+assert y_batch.shape == (2, 10), "y_batch shape is incorrect"
+x_batch, y_batch = get_batch(vectorized_songs, seq_length=5, batch_size=1)
+
+for i, (input_idx, target_idx) in enumerate(zip(x_batch[0], y_batch[0])):
+    print("Step {:3d}".format(i))
+    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx.item()])))
+    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx.item()])))
