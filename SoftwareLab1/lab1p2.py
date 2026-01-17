a6ce1922281528e90649282b041c8176be3d4aaf
Shashwat Murawala <murawala.shashwat@gmail.com>
2026-01-18 02:43:58 +0900

    Code for L1p2

--- SoftwareLab1/lab1p2.py
+++ SoftwareLab1/lab1p2.py
@@ -1,6 +1,4 @@
 import comet_ml
-# TODO: ENTER YOUR API KEY HERE!! instructions above
-COMET_API_KEY = ""
 
 # Import PyTorch and other relevant libraries
 import torch
@@ -45,7 +43,6 @@ def vectorize_string(string):
 vectorized_songs = vectorize_string(songs_joined)
 
 ### Batch definition to create training examples ###
-
 def get_batch(vectorized_songs, seq_length, batch_size):
     # the length of the vectorized songs string
     n = vectorized_songs.shape[0] - 1
@@ -65,8 +62,6 @@ def get_batch(vectorized_songs, seq_length, batch_size):
     return x_batch, y_batch
 
 ### Defining the RNN Model ###
-
-'''TODO: Add LSTM and Linear layers to define the RNN model using nn.Module'''
 class LSTMModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_size):
         super(LSTMModel, self).__init__()
@@ -98,15 +93,66 @@ class LSTMModel(nn.Module):
 
         out = self.fc(out)
         return out if not return_state else (out, state)
+
+### Defining the loss function ###
+cross_entropy = nn.CrossEntropyLoss() # instantiates the function
+def compute_loss(labels, logits):
+    """
+    Inputs:
+      labels: (batch_size, sequence_length)
+      logits: (batch_size, sequence_length, vocab_size)
+
+    Output:
+      loss: scalar cross entropy loss over the batch and sequence length
+    """
+
+    # Batch the labels so that the shape of the labels should be (B * L,)
+    batched_labels = labels.view(-1)
+
+    # Batch the logits so that the shape of the logits should be (B * L, V)
+    batched_logits = logits.view(-1, logits.size(-1)).float()
     
+    # Compute the cross-entropy loss using the batched next characters and predictions
+    loss = cross_entropy(batched_logits, batched_labels)
+    return loss
+
+def create_experiment():
+  # end any prior experiments
+  if 'experiment' in locals():
+    experiment.end()
+
+  # initiate the comet experiment for tracking
+  experiment = comet_ml.Experiment(
+                  api_key="",
+                  project_name="6S191_Lab1_Part2")
+  # log our hyperparameters, defined above, to the experiment
+  for param, value in params.items():
+    experiment.log_parameter(param, value)
+  experiment.flush()
+
+  return experiment
+### Hyperparameter setting and optimization ###
+
 vocab_size = len(vocab)
-embedding_dim = 256
-hidden_size = 1024
-batch_size = 8
+
+# Model parameters:
+params = dict(
+  num_training_iterations = 10000,  # Increase this to train longer
+  batch_size = 8,  # Experiment between 1 and 64
+  seq_length = 100,  # Experiment between 50 and 500
+  learning_rate = 5e-3,  # Experiment between 1e-5 and 1e-1
+  embedding_dim = 256,
+  hidden_size = 1024,  # Experiment between 1 and 2048
+)
+
+# Checkpoint location:
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "my_ckpt")
+os.makedirs(checkpoint_dir, exist_ok=True)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = LSTMModel(vocab_size, embedding_dim, hidden_size).to(device)
+model = LSTMModel(vocab_size, params["embedding_dim"], params["hidden_size"]).to(device)
 # Test the model with some sample data
 x, y = get_batch(vectorized_songs, seq_length=100, batch_size=32)
 x = x.to(device)
@@ -116,3 +162,126 @@ pred = model(x)
 sampled_indices = torch.multinomial(torch.softmax(pred[0], dim=-1), num_samples=1)
 sampled_indices = sampled_indices.squeeze(-1).cpu().numpy()
 sampled_indices
+
+### compute the loss on the predictions from the untrained model from earlier. ###
+y.shape  # (batch_size, sequence_length)
+pred.shape  # (batch_size, sequence_length, vocab_size)
+example_batch_loss = compute_loss(y, pred) # TODO
+
+# Move the model to the GPU
+model.to(device)
+
+'''TODO: instantiate an optimizer with its learning rate.
+  Checkout the PyTorch website for a list of supported optimizers.
+  https://pytorch.org/docs/stable/optim.html
+  Try using the Adam optimizer to start.'''
+optimizer = optim.Adam(model.parameters(), lr=params["learning_rate"])
+
+def train_step(x, y):
+  # Set the model's mode to train
+  model.train()
+
+  # Zero gradients for every step
+  optimizer.zero_grad()
+
+  # Forward pass
+  y_hat = model(x)
+
+  # Compute the loss
+  loss = compute_loss(y, y_hat)
+
+  # Backward pass
+  loss.backward()
+  optimizer.step()
+
+  return loss
+
+### Prediction of a generated song ###
+
+def generate_text(model, start_string, generation_length=1000):
+  # Evaluation step (generating ABC text using the learned RNN model)
+
+  '''TODO: convert the start string to numbers (vectorize)'''
+  input_idx = [char2idx[char] for char in start_string] # TODO
+  input_idx = torch.tensor([input_idx], dtype=torch.long).to(device)
+
+  # Initialize the hidden state
+  state = model.init_hidden(input_idx.size(0), device)
+
+  # Empty string to store our results
+  text_generated = []
+  tqdm._instances.clear()
+
+  for i in tqdm(range(generation_length)):
+    '''TODO: evaluate the inputs and generate the next character predictions'''
+    predictions, state = model(input_idx, state, return_state=True) # TODO
+
+    # Remove the batch dimension
+    predictions = predictions.squeeze(0)
+
+    '''TODO: use a multinomial distribution to sample over the probabilities'''
+    input_idx = torch.multinomial(torch.softmax(predictions, dim=-1), num_samples=1) # TODO
+
+    '''TODO: add the predicted character to the generated text!'''
+    # Hint: consider what format the prediction is in vs. the output
+    text_generated.append(idx2char[input_idx.item()]) # TODO
+  return (start_string + ''.join(text_generated))
+
+##################
+# Begin training!#
+##################
+
+history = []
+plotter = mdl.util.PeriodicPlotter(sec=2, xlabel='Iterations', ylabel='Loss')
+experiment = create_experiment()
+
+if hasattr(tqdm, '_instances'): tqdm._instances.clear() # clear if it exists
+for iter in tqdm(range(params["num_training_iterations"])):
+
+    # Grab a batch and propagate it through the network
+    x_batch, y_batch = get_batch(vectorized_songs, params["seq_length"], params["batch_size"])
+
+    # Convert numpy arrays to PyTorch tensors
+    x_batch = torch.tensor(x_batch, dtype=torch.long).to(device)
+    y_batch = torch.tensor(y_batch, dtype=torch.long).to(device)
+
+    # Take a train step
+    loss = train_step(x_batch, y_batch)
+
+    # Log the loss to the Comet interface
+    experiment.log_metric("loss", loss.item(), step=iter)
+
+    # Update the progress bar and visualize within notebook
+    history.append(loss.item())
+    plotter.plot(history)
+
+    # Save model checkpoint
+    if iter % 100 == 0:
+        torch.save(model.state_dict(), checkpoint_prefix)
+
+# Save the final trained model
+torch.save(model.state_dict(), checkpoint_prefix)
+experiment.flush()
+
+generated_text = generate_text(model, start_string="X", generation_length=1000) # TODO
+
+generated_songs = mdl.lab1.extract_song_snippet(generated_text)
+
+for i, song in enumerate(generated_songs):
+  # Synthesize the waveform from a song
+  waveform = mdl.lab1.play_song(song)
+
+  # If its a valid song (correct syntax), lets play it!
+  if waveform:
+    print("Generated song", i)
+    ipythondisplay.display(waveform)
+
+    numeric_data = np.frombuffer(waveform.data, dtype=np.int16)
+    wav_file_path = f"output_{i}.wav"
+    write(wav_file_path, 88200, numeric_data)
+
+    # save your song to the Comet interface -- you can access it there
+    experiment.log_asset(wav_file_path)
+
+# when done, end the comet experiment
+experiment.end()
